351f691d93c46b9c795f46e7ca152014edb6bb77
Kywalh <yann_pinaroli@yahoo.fr>
2018-12-22 11:38:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kywalh/fpucalculator/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.kywalh.fpucalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText mEditText_Carbs;
+    private EditText mEditText_Fat;
+    private EditText mEditText_Protein;
+    private EditText mEditText_CR;
+
+
+    private TextView mTextView_InsulinCarbs;
+    private TextView mTextView_TBR;
+    private TextView mTextView_InsulinFatProtein;
+    private TextView mTextView_FP_duration;
+
+    private Button mButtonCalculate;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mEditText_Carbs = findViewById(R.id.editText_Carbs);
+        mEditText_Fat = findViewById(R.id.editText_Fat);
+        mEditText_Protein = findViewById(R.id.editText_Protein);
+        mEditText_CR = findViewById(R.id.editText_CarbRatio);
+
+
+
+        mTextView_InsulinCarbs = findViewById(R.id.textview_InsulinCarbCalculated);
+        mTextView_InsulinFatProtein = findViewById(R.id.textview_InsulinFPUCalculated);
+        mTextView_FP_duration = findViewById(R.id.textview_DurationCalculated);
+        mTextView_TBR = findViewById(R.id.textview_EquivTBRvalue);
+
+
+        mButtonCalculate = findViewById(R.id.button_calculate);
+
+
+        mButtonCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Verify inputs from user and act accordingly
+
+                if (mEditText_Carbs.getText().toString().length() == 0) {
+                    mEditText_Carbs.setText("0");
+                }
+                if (mEditText_Fat.getText().toString().length() == 0) {
+                    mEditText_Fat.setText("0");
+                }
+                if (mEditText_Protein.getText().toString().length() == 0) {
+                    mEditText_Protein.setText("0");
+                }
+                if (mEditText_CR.getText().toString().length() == 0) {
+                    mEditText_CR.setText(String.valueOf(255)); // safety feature
+                }
+
+
+
+                int num2 = Integer.parseInt(mEditText_Carbs.getText().toString());
+                int num3 = Integer.parseInt(mEditText_Fat.getText().toString());
+                int num4 = Integer.parseInt(mEditText_Protein.getText().toString());
+                int num5 = Integer.parseInt(mEditText_CR.getText().toString());
+
+
+
+
+                /*Insulin for Carbs calculation based on Carbs divided by Carb Ratio */
+                double Insulin_for_Carbs = Math.round((num2 * 20.0 / num5)) / 20.0; //rounding to 0.05
+                mTextView_InsulinCarbs.setText(String.valueOf(Insulin_for_Carbs));
+
+
+
+                /*Calculation of FPU based on 9 x Fat + 4 x Protein
+                 * See  (WPTS) or ‘Warsaw’ formula for explanations  */
+
+                double FPU = ((double)num3 * 9 + (double)num4 * 4)  ;
+
+                // Calculation of insulin needed for Fat and Protein
+                double  InsulinforFP = (FPU / 100) * (10 / (double)num5); // scale the FPU with the Carb ratio
+                double rounded_InsulinFatProtein = Math.round(InsulinforFP * 20.0) / 20.0; //rounding to 0.05
+                String var1 = String.format("%.2f", rounded_InsulinFatProtein); // display with 2 digits
+                mTextView_InsulinFatProtein.setText(var1);
+
+
+
+
+
+
+                /*Calculation of duration for the Fat & protein
+                 * See  (WPTS) or ‘Warsaw’ formula for explanations  */
+
+                double FPUduration = 0;
+                if (InsulinforFP == 0 )
+                    FPUduration = 0;                // 0 hours
+                else if (InsulinforFP <= 1 )
+                    FPUduration = 180;              // 3 hours
+                else if (InsulinforFP <= 2 )
+                    FPUduration = 240;              // 4 hours
+                else if (InsulinforFP <= 3 )
+                    FPUduration = 300;              // 5 hours
+                else if (InsulinforFP > 3 )
+                    FPUduration = 480;              // 8 hours
+                else
+                    FPUduration = 0;                // Default and safe 0 hours
+
+                mTextView_FP_duration.setText(String.valueOf((long)FPUduration));
+
+
+                /*Calculation of equivalent additional basal rate for the same duration */
+
+                double TBRequiv = InsulinforFP / (FPUduration / 60);
+                double rounded_TBRequiv = Math.round(TBRequiv * 20.0) / 20.0; //rounding to 0.05
+                String var2 = String.format("%.2f", rounded_TBRequiv); // display with 2 digits
+                mTextView_TBR.setText(String.valueOf(var2));
+
+
+
+
+            }
+        });
+
+    }
+}
